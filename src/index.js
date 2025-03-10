38d3659ddbee73f83818401103c16af250a23293
Will Liao <wyliao76@gmail.com>
2025-03-10 15:22:35 -0700

    remove node_modules

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+require('dotenv').config({ path: `${__dirname}/.env.${process.env.NODE_ENV}` })
+const cors = require('cors')
+const path = require('path')
+
+const DatabaseManager = require('./database')
+const AIManager = require('./ai')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, '../frontend')))
+
+const dbManager = new DatabaseManager()
+dbManager.dbConnect()
+
+const aiManager = new AIManager()
+
+app.get('/api/users', async (req, res) => {
+    const users = await dbManager.User.find()
+    res.json(users)
+})
+
+app.post('/api/questions', async (req, res) => {
+    const response = await aiManager.generateQuestionBatch()
+    res.json(response)
+})
+
+app.post('/api/persona', async (req, res) => {
+    const answerObjs = req.body
+    const response = await aiManager.generatePersona(JSON.stringify(answerObjs))
+    res.json(response)
+})
+
+app.post('/api/image', async (req, res) => {
+    const answerObjs = req.body
+    const response = await aiManager.generateImage(JSON.stringify(answerObjs))
+    console.log('response being sent: ' + response)
+    res.json(response)
+})
+
+app.post('/users', async (req, res) => {
+    const newUser = new User(req.body)
+    await newUser.save()
+    res.status(201).send('User created!')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'))
+})
+
+app.get('/questions', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/questions.html'))
+})
+
+
+app.listen(process.env.port || 3000, () => console.log('Server purring on port 3000'))
