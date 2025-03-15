aa73535549a166b0c54e22c1b74011c33ab13b40
Will Liao <wyliao76@gmail.com>
2025-03-15 02:04:50 -0700

    controller unit test done

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,4 +1,5 @@
 const { adminService } = require('../services/index')
+const Joi = require('joi')
 
 const usersGET = async (req, res, next) => {
     try {
@@ -10,6 +11,77 @@ const usersGET = async (req, res, next) => {
     }
 }
 
+const banUserPOST = async (req, res, next) => {
+    try {
+        const { email = '' } = req.body
+
+        const schema = Joi.object({
+            email: Joi.string().max(24).required().email().messages({
+                'string.email': 'Not a valid email',
+                'string.max': 'Emails have a maximum length of 24 characters',
+                'string.empty': 'Email cannot be empty',
+            }),
+        })
+        await schema.validateAsync({ email }, { abortEarly: false })
+
+        const result = await adminService.banUserPOST(email)
+
+        return res.status(200).json({ msg: result })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const unBanUserPOST = async (req, res, next) => {
+    try {
+        const { email = '' } = req.body
+
+        const schema = Joi.object({
+            email: Joi.string().max(24).required().email().messages({
+                'string.email': 'Not a valid email',
+                'string.max': 'Emails have a maximum length of 24 characters',
+                'string.empty': 'Email cannot be empty',
+            }),
+        })
+        await schema.validateAsync({ email }, { abortEarly: false })
+
+        const result = await adminService.unBanUserPOST(email)
+
+        return res.status(200).json({ msg: result })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const adjustTokenPOST = async (req, res, next) => {
+    try {
+        const { email = '', times } = req.body
+
+        const schema = Joi.object({
+            email: Joi.string().max(24).required().email().messages({
+                'string.email': 'Not a valid email',
+                'string.max': 'Emails have a maximum length of 24 characters',
+                'string.empty': 'Email cannot be empty',
+            }),
+            times: Joi.number().required().messages({
+                'number.base': 'Times must be a number',
+                'number.empty': 'Times cannot be empty',
+                'any.required': 'Times is required',
+            }),
+        })
+        await schema.validateAsync({ email, times }, { abortEarly: false })
+
+        const result = await adminService.adjustTokenPOST(email, times)
+
+        return res.status(200).json({ msg: result })
+    } catch (error) {
+        next(error)
+    }
+}
+
 module.exports = {
     usersGET,
+    banUserPOST,
+    unBanUserPOST,
+    adjustTokenPOST,
 }
