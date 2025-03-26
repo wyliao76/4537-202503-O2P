4495cc138b2e1d26dc42184be2a00742e9466ca7
Ryan Lok <rlok.pc@gmail.com>
2025-03-25 21:38:13 -0700

    Write apiLogger middleware

---
+++ src/utilities/apiLogger.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const Record = require('../models/records')
+
+const apiLogger = async (req, _, next) => {
+    try {
+        const { token } = req.cookies
+
+        const result = await jwt.verify(token, process.env.SECRET)
+        const email = result.email
+        const method = req.method
+        const path = req.path
+
+        const record = new Record({
+            method: method,
+            path: path,
+            email: email,
+        })
+        await record.save()
+
+        next()
+    } catch (error) {
+        console.log('Error writing api call record')
+        next()
+    }
+}
+
+module.exports = {
+    apiLogger,
+}
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -2,11 +2,13 @@ const { CustomError } = require('./customError')
 const auth = require('./auth')
 const redis = require('./redis')
 const { AIManager } = require('./ai')
+const { apiLogger } = require('./apiLogger')
 
 module.exports = {
     CustomError,
     auth,
     redis,
     AIManager,
+    apiLogger,
 }
 
