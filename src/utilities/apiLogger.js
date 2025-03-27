dbc725f125c3eb8a7c4606d4af4c7e982014d5cb
Ryan Lok <rlok.pc@gmail.com>
2025-03-26 18:31:24 -0700

    Swap to using res.on instead of middleware for api logger

--- src/utilities/apiLogger.js
+++ src/utilities/apiLogger.js
@@ -1,29 +1,32 @@
 const jwt = require('jsonwebtoken')
 const Record = require('../models/records')
 
-const apiLogger = async (req, _, next) => {
-    try {
-        const { token } = req.cookies
+const apiLogger = (req, res, next) => {
+    res.on('finish', async () => {
+        console.log('start')
+        if (![400, 401, 403, 404].includes(res.statusCode)) {
+            try {
+                const { token } = req.cookies
 
-        const result = await jwt.verify(token, process.env.SECRET)
-        const email = result.email
-        const method = req.method
-        const path = req.path
+                const result = await jwt.verify(token, process.env.SECRET)
+                const email = result.email
+                const method = req.method
+                const path = req.path
 
-        const record = new Record({
-            method: method,
-            route: path,
-            email: email,
-        })
-        await record.save()
-        console.log('API record logged')
-
-        next()
-    } catch (error) {
-        console.log(error)
-        console.log('Error writing api call record')
-        next()
-    }
+                const record = new Record({
+                    method: method,
+                    route: path,
+                    email: email,
+                })
+                await record.save()
+                console.log('API record logged')
+            } catch (error) {
+                console.log(error)
+                console.log('Error writing api call record')
+            }
+        }
+    })
+    next()
 }
 
 module.exports = {
