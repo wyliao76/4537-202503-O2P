7d890fa6b70b4297235bf2378aa88b3f6eae8542
LouiseYLi <louiseli.van@gmail.com>
2025-03-28 12:57:26 -0700

    removed unused routes

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -6,10 +6,6 @@ router.get('/', adminController.isAdminGET)
 
 router.get('/users', adminController.usersGET)
 
-router.post('/banUser', adminController.banUserPOST)
-
-router.post('/unBanUser', adminController.unBanUserPOST)
-
 router.patch('/toggleBanUser', adminController.toggleBanUserPATCH)
 
 router.post('/adjustToken', adminController.adjustTokenPOST)
